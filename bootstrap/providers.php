e26b6fcc056fb37ccd81037b50bbeae1aa45d15f
fikrihaikal17 <fikrihaikal170308@gmail.com>
2026-02-22 07:42:02 +0700

    error bootstrap

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
   App\Providers\AppServiceProvider::class,
-  App\Providers\Filament\AdminPanelProvider::class,
   App\Providers\Filament\GuruPanelProvider::class,
   App\Providers\Filament\KesiswaanPanelProvider::class,
   App\Providers\Filament\SiswaPanelProvider::class,
